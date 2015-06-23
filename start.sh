eef05b2b3ecfb5fbbecd27091c1acd634d3e3200
Carlos M. Martinez <carlos@xt6.us>
2015-06-23 15:30:34 +0000

    commit

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 # Boot bind-based signer
 WD=$(pwd)
 echo Booting signer, with base $WD, mounted on /v
-/usr/bin/docker run -d -p 53:53 -p 53:53/udp -v $WD:/v cm2c/nssigner /opt/bbsigner/sbin/named -g -d1 -c /v/dfiles/nssigner/named.conf
+#
+/usr/bin/docker run -d --name=cm2c_signer -p 53:53 -p 53:53/udp -v $WD:/v cm2c/nssigner \
+	 /opt/bbsigner/sbin/named -g -d1 -c /v/dfiles/nssigner/named.conf
